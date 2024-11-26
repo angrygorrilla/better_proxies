0768abac78eb2a4ff883a4ac03b8a45da830f722
gchapm02@mail.uoguelph.ca <gchapm02@mail.uoguelph.ca>
2024-11-26 14:34:05 -0500

    adjusted sizing

--- Pdf_write.js
+++ Pdf_write.js
@@ -9,8 +9,11 @@ async function generate_pdf(generator){
 
   console.log('in pdf function')
 
-  const card_width=180
-  const card_height=252
+  //in pdf sizing, 72 points=1 inch
+  let x_margin=32 // 0.5 inches
+  let y_margin=104 //1.5 inches
+  const card_width=180  //2.5 inches
+  const card_height=252  //3.5 inches
 
 
   let position=0
@@ -24,8 +27,8 @@ async function generate_pdf(generator){
     let x_pos=(position%3)
     let y_pos=(Math.floor(position/3))
 
-    let cur_x=32+(x_pos*card_width)
-    let cur_y=32+(y_pos*card_height)
+    let cur_x=x_margin+(x_pos*card_width)
+    let cur_y=y_margin+(y_pos*card_height)
 
     doc.image(result, cur_x, cur_y, {fit: [card_width,card_height]})
     position=position+1
@@ -57,8 +60,9 @@ async function* yield_burgeoning(num) {
 
 
 function test_generator(){
-  const gen=yield_burgeoning(15);
+  const gen=yield_burgeoning(8);
 //generatePDFWithImage(gen);
   generate_pdf(gen);
 }
+//test_generator()
 
